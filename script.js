941f7934720e118f8b67f656bb5a104e8839f33f
Aaron Germaine <vcnts0@gmail.com>
2021-01-13 17:03:44 -0700

    Added variables and psuedocode

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Character Variables
+var lowerCase = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
+
+var upperCase = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+
+var numbers = ("1","2","3","4","5","6","7","8","9","0");
+
+var specialChar = ("!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\",","^","_","`","{","|","}","~");
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -12,3 +22,13 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+if user wants specialChar add it 
+
+if user wants numbers add numbers
+
+if user wants upperCase add it
+
+generatePassword
